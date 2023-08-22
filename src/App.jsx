0f64aea0d2735e9cebc396c1401b2d866f9dcba3
Amir Maleki <realmalekiamir@gmail.com>
2023-08-22 18:29:14 +0330

    Create a data file in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,47 @@
 import { TodoBoard, Header } from './components';
 
 function App() {
+  const data = {
+    todo: [
+      {
+        id: 1,
+        task: 'Start with meditation, exercise & breakfast for a productive day',
+      },
+      {
+        id: 2,
+        task: 'Read to learn something new every day',
+      },
+      {
+        id: 3,
+        task: 'Learn something fresh & relevant',
+      },
+    ],
+    doing: [
+      {
+        id: 1,
+        task: 'Engage & question in meetings',
+      },
+      {
+        id: 2,
+        task: 'Use time-blocking for effective days',
+      },
+    ],
+    done: [
+      {
+        id: 1,
+        task: 'Finished online course - check!',
+      },
+      {
+        id: 2,
+        task: 'Congratulate yourself for incorporating healthier habits into your lifestyle, like regular exercise or mindful eating',
+      },
+    ],
+  };
   return (
     <div className="w-[1060px] h-[862px] mt-[70px] mx-auto">
       <Header />
       <div className="flex justify-between mt-[46px]">
-        <TodoBoard />
+        <TodoBoard tasks={data.todo} />
       </div>
     </div>
   );
