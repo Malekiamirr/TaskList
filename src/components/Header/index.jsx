8239e7e5114818ebf7e710cb2ef777f916eb1832
Amir Maleki <realmalekiamir@gmail.com>
2023-08-22 08:15:39 +0330

    complete Header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,13 @@
 function Header() {
   return (
-    <header>
-      <h1 className="text-[34px]">✔️ Task List</h1>
+    <header className="w-[768px] h-[116px] space-y-[15px]">
+      <h1 className="text-[34px] font-bold leading-[41.15px]">✔️ Task List</h1>
+      <p className="leading-[30px] text-base font-medium">
+        Break your life to simple tasks to get things done!
+        <br />
+        Does not matter how many tasks you done, It’s important to break to
+        small tasks and be on progress.
+      </p>
     </header>
   );
 }
