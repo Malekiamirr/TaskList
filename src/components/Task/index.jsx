2191d6f0b29aaa36162a5b6ede2e216fd812d6fb
Amir Maleki <realmalekiamir@gmail.com>
2023-08-22 16:33:42 +0330

    Install react-icons package

--- src/components/Task/index.jsx
+++ src/components/Task/index.jsx
@@ -1,5 +1,35 @@
+import { useState } from 'react';
+
+// eslint-disable-next-line react/prop-types
 function Task({ borderColor, text }) {
-  return <div>Task</div>;
+  const [inputValue, setInputValue] = useState(text);
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
+  return (
+    <div
+      style={{ border: `1px solid ${borderColor}` }}
+      className="w-[300px] rounded py-[10px] px-[10px] bg-white flex items-center gap-2"
+    >
+      <div className="w-4 h-4">
+        <input
+          type="checkbox"
+          className="w-4 h-4 flex items-center justify-center"
+        />
+      </div>
+      <div className="w-full">
+        <input
+          value={inputValue}
+          className="text-xs font-semibold leading-[14.52px] w-full break-all outline-none"
+          onChange={handleChange}
+        />
+      </div>
+
+      {/* remove button */}
+      <div></div>
+    </div>
+  );
 }
 
 export default Task;
--- src/components/TodoBoard/index.jsx
+++ src/components/TodoBoard/index.jsx
@@ -1,14 +1,33 @@
-import { BoardTitle } from '../index';
+import { BoardTitle, Task } from '../index';
 
 function TodoBoard() {
   return (
     <div className="w-[340px] h-[700px] rounded-[10px] p-5 pb-[30px] bg-[#FEF4F3]">
+      {/* Title */}
       <BoardTitle
         title={'Todo'}
         titleColor={'text-[#6E1E29]'}
         taskColor={'text-[#D4AFB4]'}
         taskCount={3}
       />
+
+      {/* Tasks List */}
+      <div className="flex flex-col mt-5 gap-3">
+        <Task
+          text={
+            'Start with meditation, exercise & breakfast for a productive day'
+          }
+          borderColor={'#F3E1DF'}
+        />
+        <Task
+          text={'Read to learn something new every day'}
+          borderColor={'#F3E1DF'}
+        />
+        <Task
+          text={'Learn something fresh & relevant'}
+          borderColor={'#F3E1DF'}
+        />
+      </div>
     </div>
   );
 }
