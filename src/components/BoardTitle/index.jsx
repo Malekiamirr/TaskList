8a64eeb77c8b378e5353459573dff07f038b8e99
Amir Maleki <realmalekiamir@gmail.com>
2023-08-22 08:55:36 +0330

    Create BoardTitle component

---
+++ src/components/BoardTitle/index.jsx
@@ -0,0 +1,8 @@
+// eslint-disable-next-line react/prop-types
+function BoardTitle({ title, titleColor, taskColor, taskCount }) {
+  return (
+    <div className="w-[300px] h-[18px] flex items-center justify-between"></div>
+  );
+}
+
+export default BoardTitle;
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as Header } from './Header/index';
 export { default as TodoBoard } from './TodoBoard/index';
+export { default as BoardTitle } from './BoardTitle/index';
