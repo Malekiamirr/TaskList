3babad5fbe95dae472f1087d0d4522bf6ec0a2e4
Amir Maleki <realmalekiamir@gmail.com>
2023-08-22 08:58:06 +0330

    Complete BoardTitle component

--- src/components/BoardTitle/index.jsx
+++ src/components/BoardTitle/index.jsx
@@ -1,7 +1,16 @@
 // eslint-disable-next-line react/prop-types
 function BoardTitle({ title, titleColor, taskColor, taskCount }) {
   return (
-    <div className="w-[300px] h-[18px] flex items-center justify-between"></div>
+    <div className="w-[300px] h-[18px] flex items-center justify-between">
+      <h3
+        className={`${titleColor} font-semibold text-[15px] leading-[18.15px]`}
+      >
+        {title}
+      </h3>
+      <span className={`${taskColor} font-medium text-xs leading-[14.25px]`}>
+        {taskCount} Tasks
+      </span>
+    </div>
   );
 }
 
--- src/components/TodoBoard/index.jsx
+++ src/components/TodoBoard/index.jsx
@@ -1,6 +1,15 @@
+import { BoardTitle } from '../index';
+
 function TodoBoard() {
   return (
-    <div className="w-[340px] h-[700px] rounded-[10px] p-5 pb-[30px] bg-[#FEF4F3]"></div>
+    <div className="w-[340px] h-[700px] rounded-[10px] p-5 pb-[30px] bg-[#FEF4F3]">
+      <BoardTitle
+        title={'Todo'}
+        titleColor={'text-[#6E1E29]'}
+        taskColor={'text-[#D4AFB4]'}
+        taskCount={3}
+      />
+    </div>
   );
 }
 
