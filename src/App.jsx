97e5be8ad267daff6a5ec6af25df50e6057e8778
Amir Maleki <realmalekiamir@gmail.com>
2023-08-22 08:53:37 +0330

    Create TodoBoard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-import { Header } from './components';
+import { Board, Header } from './components';
 
 function App() {
   return (
     <div className="w-[1060px] h-[862px] mt-[70px] mx-auto">
       <Header />
+      <div className="flex justify-between mt-[46px]">
+        <Board />
+        <Board />
+        <Board />
+      </div>
     </div>
   );
 }
---
+++ src/components/TodoBoard/index.jsx
@@ -0,0 +1,7 @@
+function TodoBoard() {
+  return (
+    <div className="w-[340px] h-[700px] rounded-[10px] p-5 pb-[30px] bg-[#FEF4F3]"></div>
+  );
+}
+
+export default TodoBoard;
